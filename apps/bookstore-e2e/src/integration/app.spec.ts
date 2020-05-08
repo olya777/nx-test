099bd729a29ac78549b9f5dd5088cd3a5c4d7bb3
olya777 <olya777@gmail.com>
2020-05-07 23:14:00 -0700

    fixed unit test

--- apps/bookstore-e2e/src/integration/app.spec.ts
+++ apps/bookstore-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('bookstore', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Bookstore!');
+    getGreeting().contains('Bookstore');
   });
 });
